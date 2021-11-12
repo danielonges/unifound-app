b5c02b6b5f6f43934c6e4f79d177a30d6ddfc520
merylseow <71954278+merylseow@users.noreply.github.com>
2021-11-12 13:55:01 +0800

    removed buttons from profile, chats

--- unifound-client/src/pages/MyStudyListings.js
+++ unifound-client/src/pages/MyStudyListings.js
@@ -54,34 +54,34 @@ const TABLE_HEAD = [
 
 // ----------------------------------------------------------------------
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
+// function descendingComparator(a, b, orderBy) {
+//   if (b[orderBy] < a[orderBy]) {
+//     return -1;
+//   }
+//   if (b[orderBy] > a[orderBy]) {
+//     return 1;
+//   }
+//   return 0;
+// }
 
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
+// function getComparator(order, orderBy) {
+//   return order === 'desc'
+//     ? (a, b) => descendingComparator(a, b, orderBy)
+//     : (a, b) => -descendingComparator(a, b, orderBy);
+// }
 
-function applySortFilter(array, comparator, query) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
-  }
-  return stabilizedThis.map((el) => el[0]);
-}
+// function applySortFilter(array, comparator, query) {
+//   const stabilizedThis = array.map((el, index) => [el, index]);
+//   stabilizedThis.sort((a, b) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) return order;
+//     return a[1] - b[1];
+//   });
+//   if (query) {
+//     return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+//   }
+//   return stabilizedThis.map((el) => el[0]);
+// }
 
 export default function MyStudyListings() {
   const [page, setPage] = useState(0);
@@ -95,11 +95,11 @@ export default function MyStudyListings() {
   const studyBuddyContext = useContext(StudyBuddyContext);
   const { getStudyListingOfUser, studyBuddyListings } = studyBuddyContext;
 
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
+  // const handleRequestSort = (event, property) => {
+  //   const isAsc = orderBy === property && order === 'asc';
+  //   setOrder(isAsc ? 'desc' : 'asc');
+  //   setOrderBy(property);
+  // };
 
   useEffect(() => {
     getStudyListingOfUser(user.id);
@@ -161,16 +161,16 @@ export default function MyStudyListings() {
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - studyBuddyListings.length) : 0;
 
-  const filteredUsers = applySortFilter(
-    studyBuddyListings,
-    getComparator(order, orderBy),
-    filterName
-  );
+  // const filteredUsers = applySortFilter(
+  //   studyBuddyListings,
+  //   getComparator(order, orderBy),
+  //   filterName
+  // );
 
-  const isUserNotFound = filteredUsers.length === 0;
+  // const isUserNotFound = filteredUsers.length === 0;
 
   return (
-    <Page title="User | Minimal-UI">
+    <Page title="Unifound: User">
       <Container sx={{ display: 'flex', flexDirection: 'row' }}>
         <Container>
           <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
@@ -202,11 +202,11 @@ export default function MyStudyListings() {
           </Stack>
 
           <Card>
-            <UserListToolbar
+            {/* <UserListToolbar
               numSelected={selected.length}
               filterName={filterName}
               onFilterName={handleFilterByName}
-            />
+            /> */}
 
             <Scrollbar>
               <TableContainer sx={{ minWidth: 800 }}>
@@ -217,7 +217,7 @@ export default function MyStudyListings() {
                     headLabel={TABLE_HEAD}
                     rowCount={studyBuddyListings.length}
                     numSelected={selected.length}
-                    onRequestSort={handleRequestSort}
+                    // onRequestSort={handleRequestSort}
                     onSelectAllClick={handleSelectAllClick}
                   />
                   <TableBody>
@@ -262,15 +262,13 @@ export default function MyStudyListings() {
                       </TableRow>
                     )}
                   </TableBody>
-                  {isUserNotFound && (
-                    <TableBody>
-                      <TableRow>
-                        <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                          <SearchNotFound searchQuery={filterName} />
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  )}
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        {/* <SearchNotFound searchQuery={filterName} /> */}
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
                 </Table>
               </TableContainer>
             </Scrollbar>
